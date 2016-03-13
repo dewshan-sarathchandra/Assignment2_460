a5f53c1bc95427112d042479e9c2c19f44772a0f
amy92 <amrita92singh@gmail.com>
2016-03-13 16:27:40 -0400

    Update functions.php
    need to change the directory paths for the images

--- functions.php
+++ functions.php
@@ -134,26 +134,26 @@ function abc_sushi_scripts() {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'abc_sushi_scripts' );
-
+/* all the functions below are creating errors */
 /**
  * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
+ 
+require get_template_directory() . '/inc/custom-header.php';*/
 
 /**
  * Custom template tags for this theme.
- */
-require get_template_directory() . '/inc/template-tags.php';
+ 
+require get_template_directory() . '/inc/template-tags.php';*/
 
 /**
  * Custom functions that act independently of the theme templates.
- */
-require get_template_directory() . '/inc/extras.php';
+ 
+require get_template_directory() . '/inc/extras.php'; */
 
 /**
  * Customizer additions.
- */
-require get_template_directory() . '/inc/customizer.php';
+ 
+require get_template_directory() . '/inc/customizer.php'; */
 
 /**
  * Load Jetpack compatibility file.
