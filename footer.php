88f7ee423677cffe137c34bfcf0855cdcaa5d369
karibrown <kariann.brown16@gmail.com>
2016-03-14 12:57:07 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,14 +17,14 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'abc-sushi' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'abc-sushi' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'abc-sushi' ), 'abc-sushi', '<a href="http://underscores.me/" rel="designer">Kariann Brown, Amrita Singh, Dewshan Sarathchndra</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'artry-sushi' ), 'abc-sushi', '<a href="http://underscores.me/" rel="designer">Kariann Brown, Amrita Singh, Dewshan Sarathchndra</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
 <script type="text/javascript"> jQuery(document).ready(function($) {
-$.backstretch("<?php echo get_stylesheet_directory_uri(); ?>/img/IMAGECHOICE.jpg");
+$.backstretch("<?php echo get_stylesheet_directory_uri(); ?>/img/background.jpg");
 });
 </script>
 
