db356fa8f85769b600172db96ee8ddf7769ec6e6
dewshan-sarathchandra <sarathcd@sheridancollege.ca>
2016-03-15 13:45:48 -0400

    Update functions.php

--- functions.php
+++ functions.php
@@ -88,6 +88,43 @@ function abc_sushi_setup() {
 endif;
 add_action( 'after_setup_theme', 'abc_sushi_setup' );
 
+
+
+// registering custom post
+function wp_create_post_type() {
+	// setting up labels
+	$labels = array(
+ 		'name' => 'Products',
+    	'singular_name' => 'Product',
+    	'add_new' => 'Add New Product',
+    	'add_new_item' => 'Add New Product',
+    	'edit_item' => 'Edit Product',
+    	'new_item' => 'New Product',
+    	'all_items' => 'All Products',
+    	'view_item' => 'View Product',
+    	'search_items' => 'Search Products',
+    	'not_found' =>  'No Products Found',
+    	'not_found_in_trash' => 'No Products found in Trash', 
+    	'parent_item_colon' => '',
+    	'menu_name' => 'Products',
+    );
+    //registering the post type
+	register_post_type( 'product', array(
+		'labels' => $labels,
+		'has_archive' => true,
+ 		'public' => true,
+		'supports' => array( 'title', 'editor', 'excerpt', 'custom-fields', 'thumbnail','page-attributes' ),
+		'taxonomies' => array( 'post_tag', 'category' ),	
+		'exclude_from_search' => false,
+		'capability_type' => 'post',
+		'rewrite' => array( 'slug' => 'products' ),
+		)
+	);
+}
+add_action( 'init', 'wp_create_post_type' );
+
+
+
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
  *
